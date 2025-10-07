13c69b54c528e227427da84df734b6f79cc4cd79
Victor Wojciechowski <victor.wojciech@hes-so.ch>
2025-10-07 09:55:13 +0200

    changed order of projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,13 @@
-import entretien from '../assets/entretien_resized.png';
-import breitling from '../assets/macbook_breitling.png';
-import lego from '../assets/macbook_lego_resized.png';
-import wordle from '../assets/wordle_iphone_resized.png';
-import aeroDark from '../assets/dark_aero.png';
-import aeroLight from '../assets/light_aero.png';
-import aeroIpad from '../assets/aero_ipad.png'
-import aeroLastPage from '../assets/aero.png'
+import entretien from "../assets/entretien_resized.png";
+import breitling from "../assets/macbook_breitling.png";
+import lego from "../assets/macbook_lego_resized.png";
+import wordle from "../assets/wordle_iphone_resized.png";
+import aeroDark from "../assets/dark_aero.png";
+import aeroLight from "../assets/light_aero.png";
+import aeroIpad from "../assets/aero_ipad.png";
+import aeroLastPage from "../assets/aero.png";
 
 export const projects = [
-  {
-    id: "lego-story",
-    title: "Lego Story",
-    year: "2025",
-    category: "Dataviz • Frontend",
-    type: "screenshot",
-    image: lego,
-    techStack: ["HTML5", "CSS3", "JavaScript", "Vite", "D3.js"],
-    notes: [
-      "Projet réalisé en Vanilla JS et à l'aide de librairies comme D3.js.",
-    ],
-    links: {
-      code: "https://github.com/ViElChompo/visualDon-legoStory.git",
-      demo: "https://visualdon-legostory.netlify.app/",
-    },
-  },
   {
     id: "entretien-piege",
     title: "Entretien Piégé",
@@ -31,7 +15,14 @@ export const projects = [
     category: "Laravel & Vue.js • Full Stack",
     type: "screenshot",
     image: entretien,
-    techStack: ["Laravel", "Laravel Sanctum", "SQLite", "Vue.js", "Pinia", "Axios"],
+    techStack: [
+      "Laravel",
+      "Laravel Sanctum",
+      "SQLite",
+      "Vue.js",
+      "Pinia",
+      "Axios",
+    ],
     notes: [
       "Conception de l’API REST v1 + auth Sanctum",
       "Seeding + route admin protégée",
@@ -81,6 +72,22 @@ export const projects = [
     },
   },
   {
+    id: "lego-story",
+    title: "Lego Story",
+    year: "2025",
+    category: "Dataviz • Frontend",
+    type: "screenshot",
+    image: lego,
+    techStack: ["HTML5", "CSS3", "JavaScript", "Vite", "D3.js"],
+    notes: [
+      "Projet réalisé en Vanilla JS et à l'aide de librairies comme D3.js.",
+    ],
+    links: {
+      code: "https://github.com/ViElChompo/visualDon-legoStory.git",
+      demo: "https://visualdon-legostory.netlify.app/",
+    },
+  },
+  {
     id: "Aero",
     title: "Aero2 website",
     year: "2025",
@@ -88,10 +95,22 @@ export const projects = [
     type: "gallery",
     cover: aeroDark,
     images: [
-      { src: aeroDark, alt: "Page d’accueil présentant la plateforme Aero2 et son concept : une aide innovante pour optimiser l’aérodynamisme et la dépense énergétique des cyclistes." },
-      { src: aeroLight, alt: "Interface centrale permettant d’analyser l’impact réel d’un équipement sur la performance : watts économisés, compatibilité et confort." },
-      { src: aeroIpad, alt: "Espace utilisateur avec modélisation 3D personnalisée, regroupant mensurations, vélo et recommandations adaptées pour un suivi précis." },
-      {src : aeroLastPage, alt: "Système de recommandations personnalisées : en fonction des mensurations et du vélo de l’utilisateur, Aero2 propose les pièces les plus adaptées pour optimiser l’aérodynamisme, le confort et l’économie d’énergie."}
+      {
+        src: aeroDark,
+        alt: "Page d’accueil présentant la plateforme Aero2 et son concept : une aide innovante pour optimiser l’aérodynamisme et la dépense énergétique des cyclistes.",
+      },
+      {
+        src: aeroLight,
+        alt: "Interface centrale permettant d’analyser l’impact réel d’un équipement sur la performance : watts économisés, compatibilité et confort.",
+      },
+      {
+        src: aeroIpad,
+        alt: "Espace utilisateur avec modélisation 3D personnalisée, regroupant mensurations, vélo et recommandations adaptées pour un suivi précis.",
+      },
+      {
+        src: aeroLastPage,
+        alt: "Système de recommandations personnalisées : en fonction des mensurations et du vélo de l’utilisateur, Aero2 propose les pièces les plus adaptées pour optimiser l’aérodynamisme, le confort et l’économie d’énergie.",
+      },
     ],
     techStack: ["Figma", "Wireframes", "UX Research"],
   },
